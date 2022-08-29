350e50f3b19a63063be88054178784ff74a04c11
Dargis97 <dargismantas1@gmail.com>
2022-08-29 21:28:42 +0300

    sort

--- r1/src/components/006/Squares.jsx
+++ r1/src/components/006/Squares.jsx
@@ -1,21 +1,36 @@
 import React from 'react';
 import { useState } from 'react';
+import rand from '../../functions/rand';
+import randColor from '../../functions/randColor';
 
 function Squares() {
   const [sq, setSq] = useState([]);
 
   const add = () => {
-    setSq((s) => [...s, '']);
+    setSq((s) => [...s, { number: rand(100, 999), color: randColor() }]);
+  };
+
+  const sort = () => {
+    setSq((s) => [...s].sort((a, b) => b.number - a.number));
   };
 
   return (
     <>
       <div className='container'>
-        {sq.map((_, i) => (
-          <div key={i}>{i + 1}</div>
+        {sq.map((n, i) => (
+          <div
+            style={{
+              backgroundColor: n.color,
+              borderRadius: n.number % 2 ? null : '50%',
+            }}
+            key={i}
+          >
+            {n.number}
+          </div>
         ))}
       </div>
       <button onClick={add}>Add []</button>
+      <button onClick={sort}>Sort</button>
     </>
   );
 }
