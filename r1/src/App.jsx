f6b0f42bae87da6c18c9d086e027d7de04b5e40f
Dargis97 <dargismantas1@gmail.com>
2022-08-29 22:01:22 +0300

    count

--- r1/src/App.jsx
+++ r1/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <h1>State 2</h1>
         <Squares />
       </header>
     </div>
--- r1/src/components/006/Squares.jsx
+++ r1/src/components/006/Squares.jsx
@@ -14,13 +14,21 @@ function Squares() {
     setSq((s) => [...s].sort((a, b) => b.number - a.number));
   };
 
+  const filter = () => {
+    setSq((s) => [...s].filter((c) => c.number < 300));
+  };
+  const reset = () => {
+    setSq((s) => []);
+  };
+
   return (
     <>
+      <h1>State {sq.filter((s) => s.number < 300).length}</h1>
       <div className='container'>
         {sq.map((n, i) => (
           <div
             style={{
-              backgroundColor: n.color,
+              backgroundColor: n.number < 300 ? 'black' : n.color,
               borderRadius: n.number % 2 ? null : '50%',
             }}
             key={i}
@@ -29,8 +37,13 @@ function Squares() {
           </div>
         ))}
       </div>
-      <button onClick={add}>Add []</button>
-      <button onClick={sort}>Sort</button>
+
+      <div>
+        <button onClick={add}>Add []</button>
+        <button onClick={sort}>Sort</button>
+      </div>
+      <button onClick={filter}>Filter lower than 400 </button>
+      <button onClick={reset}>Reset</button>
     </>
   );
 }
